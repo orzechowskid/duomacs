e326a8cc6e385cfde6af49857d0517f02c50cce7
Dan Orzechowski <orzechod@gmail.com>
2023-01-03 14:24:48 -0500

    Update duomacs-menu.el

--- duomacs-menu.el
+++ duomacs-menu.el
@@ -308,7 +308,7 @@
 		  :keys "C-h C-a"))
     (define-key-after
       keymap [about-gnu-project]
-      '(menu-item "About GNU..." about-gnu-project
+      '(menu-item "About GNU..." describe-gnu-project
 		  :keys "C-h g"))
     keymap))
     
